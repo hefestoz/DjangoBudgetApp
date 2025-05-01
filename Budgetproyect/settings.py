0fb866232b019f00c3828e14ebdfa84aaae389d3
Chris <christiandaniel95@hotmail.com>
2025-05-01 15:44:58 -0500

    Postgress data info,  deploying

--- Budgetproyect/settings.py
+++ Budgetproyect/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import requests
+import dj_database_url
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,10 +120,7 @@ WSGI_APPLICATION = 'Budgetproyect.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
